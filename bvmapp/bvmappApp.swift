cc8d8257be7c4da4240967b17fb71c3fdbf310eb
citi94 <peterharding@me.com>
2025-07-05 19:48:47 +0100

    Initial Commit

---
+++ bvmapp/bvmappApp.swift
@@ -0,0 +1,17 @@
+//
+//  bvmappApp.swift
+//  bvmapp
+//
+//  Created by Peter Harding on 05/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct bvmappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bvmappTests/bvmappTests.swift
@@ -0,0 +1,17 @@
+//
+//  bvmappTests.swift
+//  bvmappTests
+//
+//  Created by Peter Harding on 05/07/2025.
+//
+
+import Testing
+@testable import bvmapp
+
+struct bvmappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
